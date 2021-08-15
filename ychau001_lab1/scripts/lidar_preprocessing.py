4951f7e9aa9acb7e2eb82c7840d1717e602305be
cyuanqi920 <cyuanqi920@gmail.com>
2021-08-15 22:24:09 +0800

    finished lab 1

---
+++ ychau001_lab1/scripts/lidar_preprocessing.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+def preprocess_lidar(arr):
+    # filters out inf / NaN in a numpy array
+    arr = np.array(arr)
+    arr = arr[np.isfinite(arr)]
+    return arr
+
+
+### EXAMPLE ###
+if __name__ == '__main__':
+    x = np.array([np.NaN, np.inf, 1, 2, 3])
+    print (x)
+    print (preprocess_lidar(x))
